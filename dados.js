e591d6ef3829aad4318c1196e5e057bd101f6a2c
wonca04 <70729036+wonca04@users.noreply.github.com>
2025-12-13 04:16:17 +0100

    Create dados.js

---
+++ dados.js
@@ -0,0 +1,9 @@
+window.DATOS_DADOS = {
+    acciones: [
+        "Besar", "Lamer", "Morder", "Acariciar", "Soplar", "Masajear", "Chupar", "Pellizcar suave"
+    ],
+    partes: [
+        "el cuello", "la oreja", "el pecho", "el muslo", "los labios", "la espalda",
+        "el ombligo", "los dedos de la mano", "la rodilla", "el trasero", "la cadera", "la nuca"
+    ]
+};
