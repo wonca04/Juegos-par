907eca6be1b7adea889f92b036759d981fd84dac
wonca04 <70729036+wonca04@users.noreply.github.com>
2025-12-13 04:15:50 +0100

    Create posiciones.js

---
+++ posiciones.js
@@ -0,0 +1,27 @@
+window.DATOS_POSICIONES = {
+    facil: [
+        "El Misionero", 
+        "La Cucharita", 
+        "Cara a cara sentados", 
+        "Perrito tumbado"
+    ],
+    medio: [
+        "El Perrito clásico", 
+        "El 69", 
+        "La Amazona", 
+        "De lado"
+    ],
+    dificil: [
+        "El Yunque", 
+        "La Carretilla", 
+        "De pie contra pared", 
+        "El Puente"
+    ],
+    extremo: [
+        "El Helicóptero", 
+        "El Escorpión", 
+        "Acrobacia 1", 
+        "El Águila"
+    ]
+};
+
